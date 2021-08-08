ec49f5385f373531bdf1de728ced05fef4060039
Gittenger <36045366+Gittenger@users.noreply.github.com>
2021-08-08 15:28:15 -0400

    generic toggle btn styles

--- src/components/ToggleNavButton/ToggleNavButton.styles.jsx
+++ src/components/ToggleNavButton/ToggleNavButton.styles.jsx
@@ -1,17 +1,22 @@
 import styled from 'styled-components'
 
-import { device } from '../../styles/css/utils.styles.js'
+import { device, cssIndex } from '../../styles/css/utils.styles.js'
 
 const { tabletS } = device
 
 export const ToggleNavButtonContainer = styled.button`
-	width: 1.3rem;
-	height: 1.3rem;
-	background: none;
+	${cssIndex.flexCenter}
+	width: 2rem;
+	height: 2rem;
+	background: var(--main);
+	padding-bottom: 2px;
+	padding-left: 2px;
+	padding-right: 2px;
+	border-radius: 4px;
 	cursor: pointer;
 	border: none;
 	color: var(--black);
-	transition: transform 0.4s;
+	transition: all 0.4s;
 
 	&.inactive {
 		transform: rotate(0);
@@ -19,10 +24,5 @@ export const ToggleNavButtonContainer = styled.button`
 
 	&.active {
 		transform: rotate(180deg);
-	}
-
-	${tabletS} {
-		width: 2rem;
-		height: 2rem;
 	}
 `
